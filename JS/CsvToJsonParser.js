d3927900a67a8ab426e988dbf489c2bf9c965a17
Robin Lallier <robin.lallier29@gmail.com>
2018-02-22 21:09:26 +0100

    Wallah G pas le temps

--- JS/CsvToJsonParser.js
+++ JS/CsvToJsonParser.js
@@ -5,13 +5,36 @@ class CsvToJsonParser {
 		//Déclaration des variables
 		this.planning = fs.readFileSync(csvFile, 'utf8');
 		this._planningJson = {};
-
+		this.finalJsonObject = null;
 	}
 
 
-	CsvToJsonParser.prototype.tokenize = function(data){
-		let separator = /\r\n| , /;
+	tokenize(){
+		//transforme le fichier en entrée en un tableau ou chaque case 
+		//correspond aux horaires d'un arrêt
+		let separator = /\r\n/;
+		let data = this.planning;
 		data = data.split(separator);
-		data = data.filter()
+		return data;
 	}
+
+	parse(){
+		var tData = this.tokenize();
+		//Pour chaque arrêt, parser puis retourner le nom de l'arrêt voulu
+		for (i=0; i === tData.length; i++){
+			let parsedNextLine = parseNextLine(tData[i]);
+		}
+	}
+
+	parsedNextLine(tokenizedLine){
+		let separator = /,/;
+		let line = tokenizedLine.split(separator);
+		this._planningJson[arret] = line.shift();
+		let interventionList = [];
+		while(line.length > 0){
+			
+		}
+	}
+
+
 }
